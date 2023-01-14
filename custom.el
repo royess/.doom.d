ea7b52a24d60a5458b416d2e08a3f0b35a1b8eb0
Yuxuan <yyan@duck.com>
2023-01-14 17:48:32 +0800

    adjust plugin

--- custom.el
+++ custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(org-agenda-files '("~/Seafile/My Library/org/notes/noise.org"))
  '(org-agenda-files (directory-files-recursively (getenv "ORG") "\\.org$"))
 )
 (custom-set-faces
