6bdd6da352126f6a3e941ff486eaca8b3389fd9f
Yuxuan Yan <yyan.tsinghua@gmail.com>
2024-10-23 00:07:39 +0800

    update doom and set org-journal file names

--- config.el
+++ config.el
@@ -43,6 +43,7 @@
 (setq org-directory (getenv "ORG"))
 (setq org-agenda-files (directory-files-recursively (getenv "ORG") "\\.org$"))
 (setq org-journal-file-type 'weekly)
+(setq org-journal-file-format "%Y-%m-%d.org")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
